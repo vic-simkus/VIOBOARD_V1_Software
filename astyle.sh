6c111b5160dd8fd3e45e777b5de75d374a35cea3
Vic Simkus <vic.simkus@gmail.com>
2017-12-17 09:56:24 -0600

    Updated astyle formatting and gitignore

--- astyle.sh
+++ astyle.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
-astyle -n  --indent-col1-comments  --break-blocks  --indent-preproc-block  --indent-namespaces --indent-switches  --attach-inlines --indent=tab --style=allman  --delete-empty-lines  --unpad-paren --align-pointer=type -R '*.cpp' '*.hpp'
+astyle \
+	-n \
+	-R \
+	--indent-col1-comments  \
+	--break-blocks  \
+	--indent-preproc-block  \
+	--indent-namespaces \
+	--indent-switches  \
+	--indent-preproc-define \
+	--attach-inlines \
+	--indent=force-tab \
+	--style=allman  \
+	--delete-empty-lines  \
+	--unpad-paren \
+	--align-pointer=type \
+	-Xv \
+	--pad-oper \
+	--pad-paren-in \
+	-j \
+	'*.cpp' '*.hpp'
 
 
