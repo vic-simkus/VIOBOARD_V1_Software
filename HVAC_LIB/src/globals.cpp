982b3981726631ac550d644253c22072ac12a74f
vic-simkus <vic.simkus@gmail.com>
2019-12-13 13:43:22 -0600

    Format update

--- HVAC_LIB/src/globals.cpp
+++ HVAC_LIB/src/globals.cpp
@@ -66,172 +66,172 @@ namespace BBB_HVAC
 
 		string sig_to_str( int _sig )
 		{
-			if( _sig == SIGHUP )
+			if ( _sig == SIGHUP )
 			{
 				return "SIGHUP";
 			}
 
-			if( _sig == SIGINT )
+			if ( _sig == SIGINT )
 			{
 				return "SIGINT";
 			}
 
-			if( _sig == SIGQUIT )
+			if ( _sig == SIGQUIT )
 			{
 				return "SIGQUIT";
 			}
 
-			if( _sig == SIGILL )
+			if ( _sig == SIGILL )
 			{
 				return "SIGILL";
 			}
 
-			if( _sig == SIGTRAP )
+			if ( _sig == SIGTRAP )
 			{
 				return "SIGTRAP";
 			}
 
-			if( _sig == SIGABRT )
+			if ( _sig == SIGABRT )
 			{
 				return "SIGABRT";
 			}
 
-			if( _sig == SIGIOT )
+			if ( _sig == SIGIOT )
 			{
 				return "SIGIOT";
 			}
 
-			if( _sig == SIGBUS )
+			if ( _sig == SIGBUS )
 			{
 				return "SIGBUS";
 			}
 
-			if( _sig == SIGFPE )
+			if ( _sig == SIGFPE )
 			{
 				return "SIGFPE";
 			}
 
-			if( _sig == SIGKILL )
+			if ( _sig == SIGKILL )
 			{
 				return "SIGKILL";
 			}
 
-			if( _sig == SIGUSR1 )
+			if ( _sig == SIGUSR1 )
 			{
 				return "SIGUSR1";
 			}
 
-			if( _sig == SIGSEGV )
+			if ( _sig == SIGSEGV )
 			{
 				return "SIGSEGV";
 			}
 
-			if( _sig == SIGUSR2 )
+			if ( _sig == SIGUSR2 )
 			{
 				return "SIGUSR2";
 			}
 
-			if( _sig == SIGPIPE )
+			if ( _sig == SIGPIPE )
 			{
 				return "SIGPIPE";
 			}
 
-			if( _sig == SIGALRM )
+			if ( _sig == SIGALRM )
 			{
 				return "SIGALRM";
 			}
 
-			if( _sig == SIGTERM )
+			if ( _sig == SIGTERM )
 			{
 				return "SIGTERM";
 			}
 
-			if( _sig == SIGSTKFLT )
+			if ( _sig == SIGSTKFLT )
 			{
 				return "SIGSTKFLT";
 			}
 
-			if( _sig == SIGCLD )
+			if ( _sig == SIGCLD )
 			{
 				return "SIGCLD";
 			}
 
-			if( _sig == SIGCHLD )
+			if ( _sig == SIGCHLD )
 			{
 				return "SIGCHLD";
 			}
 
-			if( _sig == SIGCONT )
+			if ( _sig == SIGCONT )
 			{
 				return "SIGCONT";
 			}
 
-			if( _sig == SIGSTOP )
+			if ( _sig == SIGSTOP )
 			{
 				return "SIGSTOP";
 			}
 
-			if( _sig == SIGTSTP )
+			if ( _sig == SIGTSTP )
 			{
 				return "SIGTSTP";
 			}
 
-			if( _sig == SIGTTIN )
+			if ( _sig == SIGTTIN )
 			{
 				return "SIGTTIN";
 			}
 
-			if( _sig == SIGTTOU )
+			if ( _sig == SIGTTOU )
 			{
 				return "SIGTTOU";
 			}
 
-			if( _sig == SIGURG )
+			if ( _sig == SIGURG )
 			{
 				return "SIGURG";
 			}
 
-			if( _sig == SIGXCPU )
+			if ( _sig == SIGXCPU )
 			{
 				return "SIGXCPU";
 			}
 
-			if( _sig == SIGXFSZ )
+			if ( _sig == SIGXFSZ )
 			{
 				return "SIGXFSZ";
 			}
 
-			if( _sig == SIGVTALRM )
+			if ( _sig == SIGVTALRM )
 			{
 				return "SIGVTALRM";
 			}
 
-			if( _sig == SIGPROF )
+			if ( _sig == SIGPROF )
 			{
 				return "SIGPROF";
 			}
 
-			if( _sig == SIGWINCH )
+			if ( _sig == SIGWINCH )
 			{
 				return "SIGWINCH";
 			}
 
-			if( _sig == SIGPOLL )
+			if ( _sig == SIGPOLL )
 			{
 				return "SIGPOLL";
 			}
 
-			if( _sig == SIGIO )
+			if ( _sig == SIGIO )
 			{
 				return "SIGIO";
 			}
 
-			if( _sig == SIGPWR )
+			if ( _sig == SIGPWR )
 			{
 				return "SIGPWR";
 			}
 
-			if( _sig == SIGSYS )
+			if ( _sig == SIGSYS )
 			{
 				return "SIGSYS";
 			}
@@ -246,7 +246,7 @@ namespace BBB_HVAC
 
 		extern void destroy_watchdog()
 		{
-			if( watchdog != nullptr )
+			if ( watchdog != nullptr )
 			{
 				watchdog->stop_thread();
 			}
@@ -256,7 +256,7 @@ namespace BBB_HVAC
 
 		void configure_watchdog( void )
 		{
-			if( watchdog == nullptr )
+			if ( watchdog == nullptr )
 			{
 				watchdog = new WATCHDOG();
 				watchdog->start_thread();
@@ -270,7 +270,7 @@ namespace BBB_HVAC
 			/*
 			 * This seems like a bad idea
 			 */
-			if( in_trap == true )
+			if ( in_trap == true )
 			{
 				return;
 			}
@@ -279,8 +279,8 @@ namespace BBB_HVAC
 			LOG_DEBUG_STAT( string( "********************************************************************" ) );
 			in_trap = true;
 			THREAD_REGISTRY::stop_all();
-			LOG_DEBUG_STAT( string( "Cleanup finished.  Exiting." ) );
 			GLOBALS::global_exit_flag = true;
+			LOG_DEBUG_STAT( string( "Cleanup finished.  Exiting." ) );
 			return;
 		}
 
@@ -290,7 +290,7 @@ namespace BBB_HVAC
 			memset( &sa, 0, sizeof( struct sigaction ) );
 			sa.sa_handler = signal_trap;
 
-			if( sigaction( SIGINT, &sa, nullptr ) != 0 ) //2
+			if ( sigaction( SIGINT, &sa, nullptr ) != 0 ) //2
 			{
 				LOG_ERROR_STAT( create_perror_string( "Failed to install signal handler for SIGINT" ) );
 			}
@@ -301,37 +301,37 @@ namespace BBB_HVAC
 			 LOG_ERROR_STAT(create_perror_string("Failed to install signal handler for SIGKILL"));
 			 }*/
 
-			if( signal( SIGPIPE, SIG_IGN ) != 0 )
+			if ( signal( SIGPIPE, SIG_IGN ) != 0 )
 			{
 				LOG_ERROR_STAT( create_perror_string( "Failed to ignore SIGPIPE" ) );
 			}
 
-			if( sigaction( SIGABRT, &sa, nullptr ) != 0 ) //6
+			if ( sigaction( SIGABRT, &sa, nullptr ) != 0 ) //6
 			{
 				LOG_ERROR_STAT( create_perror_string( "Failed to install signal handler for SIGABRT" ) );
 			}
 
-			if( sigaction( SIGFPE, &sa, nullptr ) != 0 ) //8
+			if ( sigaction( SIGFPE, &sa, nullptr ) != 0 ) //8
 			{
 				LOG_ERROR_STAT( create_perror_string( "Failed to install signal handler for SIGFPE" ) );
 			}
 
-			if( sigaction( SIGILL, &sa, nullptr ) != 0 ) //4
+			if ( sigaction( SIGILL, &sa, nullptr ) != 0 ) //4
 			{
 				LOG_ERROR_STAT( create_perror_string( "Failed to install signal handler for SIGILL" ) );
 			}
 
-			if( sigaction( SIGSEGV, &sa, nullptr ) != 0 )
+			if ( sigaction( SIGSEGV, &sa, nullptr ) != 0 )
 			{
 				LOG_ERROR_STAT( create_perror_string( "Failed to install signal handler for SIGSEGV" ) );
 			}
 
-			if( sigaction( SIGTERM, &sa, nullptr ) != 0 )
+			if ( sigaction( SIGTERM, &sa, nullptr ) != 0 )
 			{
 				LOG_ERROR_STAT( create_perror_string( "Failed to install signal handler for SIGTERM" ) );
 			}
 
-			if( sigaction( SIGHUP, &sa, nullptr ) != 0 )
+			if ( sigaction( SIGHUP, &sa, nullptr ) != 0 )
 			{
 				LOG_ERROR_STAT( create_perror_string( "Failed to install signal handler  for SIGHUP" ) );
 			}
